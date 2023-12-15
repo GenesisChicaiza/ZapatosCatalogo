d08289138bec370bb013e6b870b0f60891bcb45d
Genesis Chicaiza <genesisch234@gmail.com>
2023-12-16 00:13:55 +0100

    Terminadoo

---
+++ html/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"dominio","l":"CatalogoZapatos"},{"p":"presentacion","l":"Interfaz"},{"p":"aplicacion","l":"Main"},{"p":"dominio","l":"Producto"},{"p":"dominio","l":"Sandalia"},{"p":"dominio","l":"ZapatoDeportivo"},{"p":"dominio","l":"ZapatoElegante"}]
